22a8ebb931fdb01fa2a5538db97e68cd315bb6f9
memoryboxes <memoryboxes@gmail.com>
2014-12-09 17:30:38 +0800

    add basic container restful action

--- dockerfly/bin/dockerflyd.py
+++ dockerfly/bin/dockerflyd.py
@@ -1,11 +1,46 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import grp
+import signal
 import daemon
+import lockfile
 
 from include import dockerfly_version
 from dockerfly.http.server import run_server
 
-with daemon.DaemonContext():
-    run_server()
+def dockerflyd_setup():
+    pass
+
+def dockerflyd_cleanup():
+    pass
+
+def dockerflyd_reload_config():
+    pass
+
+context = daemon.DaemonContext(
+    working_directory='/var/lib/dockerfly',
+    umask=0o002,
+    pidfile=lockfile.FileLock('/var/run/dockerflyd.pid'),
+    )
+
+context.signal_map = {
+    signal.SIGTERM: dockerflyd_cleanup,
+    signal.SIGHUP: 'terminate',
+    signal.SIGUSR1: dockerflyd_reload_config,
+    }
+
+mail_gid = grp.getgrnam('mail').gr_gid
+context.gid = mail_gid
+
+#important_file = open('spam.data', 'w')
+#interesting_file = open('eggs.data', 'w')
+#context.files_preserve = [important_file, interesting_file]
+
+if __name__ == '__main__':
+    dockerflyd_setup()
+
+    with daemon.DaemonContext():
+        run_server()
 
--- dockerfly/http/server.py
+++ dockerfly/http/server.py
@@ -1,13 +1,37 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+import json
+from flask import Flask, request
+
+from dockerfly.bin.include import dockerfly_version
 
 dockerfly_app = Flask(__name__)
 
 @dockerfly_app.route('/')
-def hello_world():
-    return 'hello, world'
+def show_hello():
+    return json.dumps({"dockerfly":dockerfly_version})
+
+@dockerfly_app.route('/containers/edit', methods = ['POST', 'PUT', 'DELETE'])
+def containers_edit():
+    if request.method == 'POST':
+        return "ECHO: POST\n"
+
+    elif request.method == 'PUT':
+        return "ECHO: PUT\n"
+
+    elif request.method == 'DELETE':
+        return "ECHO: DELETE"
+
+@dockerfly_app.route('/containers/status', methods = ['GET'])
+def containers_status():
+    if request.method == 'GET':
+        return "ECHO: GET\n"
+
+@dockerfly_app.route('/containers/control', methods = ['POST'])
+def containers_control():
+    if request.method == 'POST':
+        return "ECHO: POST\n"
 
 def run_server():
     dockerfly_app.run()
