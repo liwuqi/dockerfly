f6919615fd4da1c622c95da37b24e8f9fa2804b8
memoryboxes <memoryboxes@gmail.com>
2015-03-18 17:53:34 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+BASE_PATH=$(cd "$(dirname "$0")"; pwd)
+LOG_FILE=$BASE_PATH/web.log
+PID_FILE=$BASE_PATH/web.pid
+
+if [ -f "$PID_FILE" ]; then
+    cat $PID_FILE | xargs kill -9
+fi
+
+ps aux|grep dockerflyd|grep -v grep|awk '{print $2}'|xargs kill -9
+python  $BASE_PATH/dockerfly/bin/dockerflyd.py
+echo "dockerflyd Server is running"
+
+nohup python $BASE_PATH/dockerflyui/server.py $1 > $LOG_FILE 2>&1 & echo $! > $PID_FILE
+echo "dockerflyui Server is running"
