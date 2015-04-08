34c50e7df4c965020253124420a74a0f332f7377
memoryboxes <memoryboxes@gmail.com>
2015-04-08 10:12:53 +0800

    upgrade docker-py to 1.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if py_version < (2, 6):
 elif (3, 0) < py_version < (3, 2):
     raise RuntimeError('On Python 3, dockerfly requires Python 3.2 or later')
 
-requires = ['sh >= 1.09', 'docker-py >= 0.6.0',
+requires = ['sh >= 1.09', 'docker-py >= 1.1.0',
             'docopt >= 0.6.1', 'flask >= 0.10.1', 'python-daemon >= 1.5.6',
             'flask-restful >= 0.3.0', 'requests >= 2.4.0']
 
